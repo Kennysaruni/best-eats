8db45aa566bb8ed9b875d5ecdfb17a36e6179fb3
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-07-14 18:07:13 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Cards from './components/Cards';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Cards/> 
+    </div>
+  );
+}
+
+export default App;
