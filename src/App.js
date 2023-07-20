9c41bd7a494b42bc0b62204e415c23f79d3575d6
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-07-20 13:19:43 +0300

    Front-end complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Cards from './components/Cards';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Food from './components/Food';
+import Category from './components/Category';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Navbar/>
       <Hero/>
       <Cards/> 
+      <Food/>
+      <Category/>
     </div>
   );
 }
